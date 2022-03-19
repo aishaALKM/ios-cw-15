668e774c8d67356e972bccadb83f7d1988c8a407
aishaALKM <70426536+aishaALKM@users.noreply.github.com>
2022-03-19 10:37:05 +0300

    classssswork15day6

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+struct Newstrrruct {
+    var name: String
+    var age: Int
+    var grade: Double
+    var atendnceeeeee: Bool
+    
+    
+}
+
+var me = Newstrrruct(name: "Aisha", age: 16, grade: 98, atendnceeeeee: true)
